02bf7c880119b6c872ede8cbe47e4a3ee33d054d
el3091 <el3091@lern261mac03.atg.columbia.edu>
2022-04-30 21:44:32 -0400

    added a bit of some random number generator

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <random>
+
+
 using namespace std;
 
 int main() 
 {
-    cout << "Hello, World!";
+
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1,6);
+    int dice_roll = distribution(generator);  // generates number in the range 1..6 
+
+    int dice_roll_2 = distribution(generator);  // generates number in the range 1..6 
+
+    cout << "Hello, World! and a dice roll: " << dice_roll << " " << dice_roll_2 << endl;
     return 0;
 }
