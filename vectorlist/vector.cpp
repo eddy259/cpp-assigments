95be14159e9a30219cdca4b2ea685674e7d744cc
el3091 <el3091@ec18mac01.atg.columbia.edu>
2022-05-01 08:07:39 -0400

    added vector add and remove

---
+++ vectorlist/vector.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<random>
+
+
+using namespace std;
+
+int main() {
+
+    int n = 20;
+
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1,10);
+
+    int dice_roll = distribution(generator);  // generates number in the range 1..6 
+
+    vector<int> num_vec;
+    for (int i = 0; i < n; i++) {
+        // add an element to vector
+        int rand = distribution(generator);
+        num_vec.push_back(rand);
+        sort(begin(num_vec), end(num_vec));
+        
+        // print out vector contents at each step
+        for (int j = 0; j < i + 1; j++) {
+            cout << num_vec[j] << " ";
+        }
+
+        cout << endl;
+        
+
+    }
+
+    cout << "REMOVING ELEMENTS: \n";
+
+    // remove elements
+    for (int i = 0; i < n; i++) {
+        int remaining_elems = n - i;
+        uniform_int_distribution<int> index_dist(1, remaining_elems);
+        int index_to_remove = index_dist(generator) - 1;
+        cout << "index to remove: " << index_to_remove << endl;
+        num_vec.erase(num_vec.begin() + index_to_remove);
+
+        // print out vector contents at each step
+        for (int j = 0; j < remaining_elems - 1; j++) {
+            cout << num_vec[j] << " ";
+        }
+
+        cout << endl;
+    }
+
+
+    return 0;
+
+}
+
+
+
+
+
