10b4af121d493d8fd69e3482fe92b4392b171a10
el3091 <el3091@ec18mac01.atg.columbia.edu>
2022-05-01 06:39:21 -0400

    added boolean and for an accumulator

--- accumulator/accumulator.cpp
+++ accumulator/accumulator.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int my_sum(int a, int b);
+int my_mult(int a, int b);  
+bool bool_and(bool a, bool b);
 
 //Accumulator sum(Container c, Operation op, Accumulator acc);
 
@@ -31,22 +33,40 @@ Accumulator sum(Container c, Operation op, Accumulator acc)
 
 int main() {
 
-   int nums[] = {1, 2, 3, 4, 5}; 
-   vector<int> num_vector;
-   
-   num_vector.push_back(1);
-   num_vector.push_back(2);
-   num_vector.push_back(3);
+    // integer addition
+    int nums[] = {1, 2, 3, 4, 5}; 
+    vector<int> num_vector;
 
-   int accumulated = sum(num_vector, my_sum, 0);
-   cout << "my accumulated: " << accumulated << endl;
+    num_vector.push_back(1);
+    num_vector.push_back(2);
+    num_vector.push_back(3);
+    num_vector.push_back(4);
+    num_vector.push_back(5);
 
+    int accumulated = sum(num_vector, my_sum, 0);
+    cout << "my accumulated: " << accumulated << endl;
 
-   return 0;
+    // integer multiplication
+    int mult_accumulated = sum(num_vector, my_mult, 1);
+    cout << "my accumulated: " << mult_accumulated << endl;
+
+    // bool and
+    vector<bool> bool_vec;
+    bool_vec.push_back(true);
+    bool_vec.push_back(false);
+    bool_vec.push_back(true);
+
+    bool and_accumulated = sum(bool_vec, bool_and, true);
+    cout << "and accumulated: " << and_accumulated << endl;
+
+    return 0;
 }
 
 
 
+int my_mult(int a, int b) {
+    return a * b;
+}
 
 
 int my_sum(int a, int b) {
@@ -54,3 +74,6 @@ int my_sum(int a, int b) {
     return a + b;
 }
 
+bool bool_and(bool a, bool b) {
+    return a * b;
+}
